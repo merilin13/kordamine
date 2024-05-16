a215f579ca15e5b6fe7412bf640b8a5116e36c53
merilin13 <merilin.hiis13@gmail.com>
2024-05-16 14:14:50 +0300

    teemant

--- forloop/Program.cs
+++ forloop/Program.cs
@@ -5,8 +5,18 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            int valik = Convert.ToInt32(Console.ReadLine());
+            switch (valik)
+            {
+                case 1:
+                    maja();
+                    break;
+                case 2:
+                    teemant();
+                    break;
 
-            maja();
+            }
+                
         }
         static void maja() 
         {
@@ -48,6 +58,36 @@
                 }
                 Console.WriteLine();
             }
+            
+        }
+        static void teemant()
+        {
+            int i, j, count = 1, number;
+            Console.Write("Enter number of rows:");
+            number = int.Parse(Console.ReadLine());
+
+            count = number - 1;
+            for (j = 1; j <= number; j++)
+            {
+                for (i = 1; i <= count; i++)
+                    Console.Write(" ");
+                count--;
+                for (i = 1; i <= 2 * j - 1; i++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+
+            count = 1;
+            for (j = 1; j <= number - 1; j++)
+            {
+                for (i = 1; i <= count; i++)
+                    Console.Write(" ");
+                count++;
+                for (i = 1; i <= 2 * (number - j) - 1; i++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+
         }
     }
 }
