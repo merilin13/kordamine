1c7872c7d92824f72673cae8ea2f0196fa4b21d4
merilin13 <merilin.hiis13@gmail.com>
2024-05-16 09:53:09 +0300

    switchcasees

---
+++ switch3/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace switch3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string option =Console.ReadLine();
+
+            switch(option)
+            {
+                case "1":
+                    Console.WriteLine("case1");
+                   break;
+
+                case "2":
+                    Console.WriteLine("case2");
+                    break;
+
+                case "3":
+                    Console.WriteLine("case3");
+                    break;
+
+                default:
+                    Console.WriteLine("slay");
+                    break;
+            }
+        }
+    }
+}
