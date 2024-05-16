80f185f9fe549c1236eabacabe64a1eb3490ce6e
merilin13 <merilin.hiis13@gmail.com>
2024-05-16 11:34:48 +0300

    while do numbers

---
+++ whiledo/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace whiledo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+            
+            do 
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine(num++);
+            }
+            while (num > 0);
+        }
+    }
+}
