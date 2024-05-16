d239dde7b175fe9a186e87c6645c8643b1541cde
merilin13 <merilin.hiis13@gmail.com>
2024-05-16 09:21:46 +0300

    code+pilt

---
+++ if and else kordamine/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace if_and_else_kordamine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int auto = 1000;
+            int rekka = 5000;
+            if (auto < rekka)
+            {
+                int vahe =(auto -rekka);
+                Console.WriteLine("auto on" + vahe + "kg kergem kui rekka");
+            }
+            else
+            {
+                Console.WriteLine("rekka on kergem");
+            }
+        }
+    }
+}
