19ffcfa1d433b59bd3416240e9ab40cbdfd23078
merilin13 <150146754+merilin13@users.noreply.github.com>
2024-05-23 10:24:56 +0300

    Add files via upload

---
+++ ristkülik/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace ristkülik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            kuju();
+        }
+
+        static void kuju()
+        {
+            Console.WriteLine("sisesta pikkus");
+            int pikkus = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("sisesta laius");
+            int laius = Convert.ToInt32(Console.ReadLine());
+            int pindala = (pikkus * laius);
+            Console.WriteLine("Pindala on " + pindala + " midagi midagi.");
+            int ümbermõõt = (2 * (pikkus + laius));
+            Console.WriteLine("Ümbermõõt on " + ümbermõõt + " midagi midagi.");
+
+            for (int row = 1; row <= pikkus; row++)
+            {
+
+                if (row == 1 || row == pikkus)
+                {
+                    for (int col = 1; col <= laius; col++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+
+
+                if (row < pikkus && row < pikkus - 1)
+                {
+                    Console.Write("*");
+                    for (int i = 0; i < laius - 2; i++)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write("*");
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
